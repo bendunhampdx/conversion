9052d1851483c95d3a31db5ee57bac2047171efe
bendunhampdx <bendunhampdx@gmail.com>
2021-09-10 11:40:49 -0700

    get conversion for UNITED EMIRATES working, and error handling

--- src/index.js
+++ src/index.js
@@ -11,12 +11,22 @@ function getElements(response) {
     const errorTypeIfExists = (response && response['error-type']) ? response['error-type'] : null;
     $('.showErrors').html(`There was an error and the type was ${errorTypeIfExists}!`)
   }
-  else if ()
+  else if (country === 'United Emirates') {
+    let AEDConversion = $('#dollar').val() * response.conversion_rates.AED;
+    $('.output').html(`The conversion in AED is ${AEDConversion}`);
+  }
+  else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
 }
 
 $(document).ready(function() {
   $('#convert').click(function() {
-
-  })
-
-})
+    CurrencyService.getCurrency()
+    .then(function (response) {
+      getElements(response);
+    }).catch(function() {
+      $('.showErrors').text(`There was an error fethcing informatin from the API.`)
+    });
+  });
+});
